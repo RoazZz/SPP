df33f7421225e02b1f4abc0df419f905b9803433
RoazZz <goliatcalixto@gmail.com>
2026-03-23 19:40:22 -0600

    Feat: Implementar capa de acceso a datos y prueba de conexión

---
+++ src/main/java/accesodatos/ConexionBD.java
@@ -0,0 +1,29 @@
+package accesodatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConexionBD {
+    protected Connection conexion;
+    private static final Logger logger = Logger.getLogger(ConexionBD.class.getName());
+
+    public ConexionBD() {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String user = "RoazAccess";
+        String contrasenia = "nugRJa1105!";
+        String basedatos = "sppbd";
+        String server = "jdbc:mysql://localhost:3306/" + basedatos;
+
+        try{
+            Class.forName(driver);
+            conexion = DriverManager.getConnection(server, user, contrasenia);
+        }catch (SQLException ex) {
+            logger.log(Level.SEVERE, "Error al establecer conexión con la base de datos", ex);
+        } catch (ClassNotFoundException ex) {
+            logger.log(Level.SEVERE, "No se encontró el driver JDBC", ex);
+        }
+    }
+}
---
+++ src/main/java/accesodatos/PruebaConexion.java
@@ -0,0 +1,13 @@
+package accesodatos;
+
+public class PruebaConexion {
+    public static void main(String[] args) {
+        ConexionBD conexion = new ConexionBD();
+
+        if (conexion.conexion != null) {
+            System.out.println("¡Conexión exitosa!");
+        } else {
+            System.err.println("Falló la conexión.");
+        }
+    }
+}
