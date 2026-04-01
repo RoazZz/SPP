c2dcfcfbe35524115563348faeeec4411e15f295
Jared <JaredMT251105@Gmail.com>
2026-04-01 17:43:47 -0600

    feat: vincular coordinador con usuario y corregir herencia de DAOs

--- src/main/java/logica/dao/CoordinadorDAO.java
+++ src/main/java/logica/dao/CoordinadorDAO.java
@@ -3,6 +3,8 @@ package logica.dao;
 import interfaces.CoordinadorDAOInterfaz;
 import accesodatos.ConexionBD;
 import logica.dto.CoordinadorDTO;
+import logica.enums.TipoDeUsuario;
+import logica.enums.TipoEstado;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,18 +13,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CoordinadorDAO extends ConexionBD implements CoordinadorDAOInterfaz{
-    private static final String SQL_INSERT  = "INSERT INTO Coordinador (NumeroDePersonal) VALUES (?)";
-    private static final String SQL_SELECT_ALL = "SELECT * FROM Coordinador";
+    private static final String SQL_INSERT  = "INSERT INTO Coordinador (idUsuario, NumeroDePersonal) VALUES (?, ?)";
+    private static final String SQL_SELECT_ALL =
+            "SELECT usuario.idUsuario, usuario.nombre, usuario.apellidoPaterno, usuario.apellidoMaterno, " +
+            "usuario.contrasenia, usuario.tipoDeUsuario, usuario.estado, " +
+            "coordinador.NumeroDePersonal" +
+            "FROM usuario JOIN coordinador ON usuario.idUsuario = coordinador.idUsuario";
+
     public CoordinadorDAO(){
         super();
     }
 
     @Override
     public void agregarCoordinador(CoordinadorDTO coordinador) throws Exception {
-        try (PreparedStatement preparedStatement = conexion.prepareStatement(SQL_INSERT)){
-            preparedStatement.setString(1, coordinador.getNumeroPersonal());
+        UsuarioDAO usuarioDAO = new UsuarioDAO(this.conexion);
+
+        try {
+            conexion.setAutoCommit(false);
+            usuarioDAO.agregarUsuario(coordinador);
+            int idGenerado = coordinador.getIdUsuario();
+
+            if (idGenerado > 0) {
+                try (PreparedStatement ps = conexion.prepareStatement(SQL_INSERT)) {
+                    ps.setInt(1, idGenerado);
+                    ps.setString(2, coordinador.getNumeroPersonal());
+                    ps.executeUpdate();
+                }
+                conexion.commit();
+            } else {
+                throw new Exception("No se pudo crear el usuario base");
+            }
         } catch (SQLException e) {
-            throw new Exception("Error al agregar el coordinador: " + e.getMessage());
+            conexion.rollback();
+            throw new Exception("Error al agregar coordinador: " + e.getMessage());
+        } finally {
+            conexion.setAutoCommit(true);
         }
     }
 
@@ -34,6 +59,13 @@ public class CoordinadorDAO extends ConexionBD implements CoordinadorDAOInterfaz
                  ResultSet resultSet = preparedStatement.executeQuery()) {
                 while(resultSet.next()) {
                     CoordinadorDTO coordinador = new CoordinadorDTO(
+                            resultSet.getInt("idUsuario"),
+                            resultSet.getString("nombre"),
+                            resultSet.getString("apellidoPaterno"),
+                            resultSet.getString("apellidoMaterno"),
+                            resultSet.getString("contrasenia"),
+                            TipoEstado.valueOf(resultSet.getString("estado")),
+                            TipoDeUsuario.valueOf(resultSet.getString("tipoDeUsuario")),
                             resultSet.getString("NumeroDePersonal")
                     );
                     listaCoordinador.add(coordinador);
--- src/main/java/logica/dto/CoordinadorDTO.java
+++ src/main/java/logica/dto/CoordinadorDTO.java
@@ -1,9 +1,13 @@
 package logica.dto;
 
-public class CoordinadorDTO {
+import logica.enums.TipoDeUsuario;
+import logica.enums.TipoEstado;
+
+public class CoordinadorDTO extends UsuarioDTO {
     private String numeroPersonal;
 
-    public CoordinadorDTO (String numeroPersonal){
+    public CoordinadorDTO (int idUsuario, String nombre, String apellidoPaterno, String apellidoMaterno, String contrasenia, TipoEstado estado, TipoDeUsuario tipoDeUsuario, String numeroPersonal){
+        super(idUsuario, nombre, apellidoPaterno, apellidoMaterno, contrasenia, estado, tipoDeUsuario);
         this.numeroPersonal = numeroPersonal;
     }
 
