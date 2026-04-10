e5f0d76c2ef4e327aa8fb052c0f1ea3b6fbb9850
Jared <JaredMT251105@Gmail.com>
2026-04-10 16:16:59 -0600

    refactor: modificar librerias en clase ActividadDAO

--- src/main/java/logica/dao/ActividadDAO.java
+++ src/main/java/logica/dao/ActividadDAO.java
@@ -6,7 +6,11 @@ import accesodatos.ConexionBD;
 import logica.dto.ActividadDTO;
 
 import java.io.IOException;
-
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
