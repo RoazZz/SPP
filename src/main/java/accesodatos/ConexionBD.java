3f221fc81c89ad35b0bad204a6eeec7ce46b2b28
RoazZz <goliatcalixto@gmail.com>
2026-04-10 13:55:08 -0600

    Feat: Implementar Singleton en ConexionBD y refactorizar al español

--- src/main/java/accesodatos/ConexionBD.java
+++ src/main/java/accesodatos/ConexionBD.java
@@ -9,45 +9,42 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ConexionBD implements AutoCloseable {
+public class ConexionBD{
+    private static ConexionBD instancia;
     private String ENLACE;
     private String USUARIO;
     private String CONTRASEÑA;
-    protected Connection conexion = null;
+    private Connection conexion = null;
 
     private static final Logger logger = Logger.getLogger(ConexionBD.class.getName());
 
-    public ConexionBD() throws IOException {
+    private ConexionBD() throws IOException, SQLException {
         Properties properties = new Properties();
         try(FileInputStream fileInputStream = new FileInputStream("config.properties")) {
             properties.load(fileInputStream);
             this.ENLACE = properties.getProperty("db.enlace");
             this.USUARIO = properties.getProperty("db.usuario");
             this.CONTRASEÑA = properties.getProperty("db.contraseña");
+            conexion = DriverManager.getConnection(ENLACE, USUARIO, CONTRASEÑA);
+            logger.log(Level.INFO, "Conexión exitosa a la base de datos");
         }catch (IOException e){
             logger.log(Level.SEVERE, "Error al leer el archivo de configuración: " + e.getMessage(), e);
+            throw e;
+        }catch (SQLException e){
+            logger.log(Level.SEVERE, "Error al conectar a la base de datos: " + e.getMessage(), e);
+            throw e;
         }
     }
 
-    public Connection conectarBD() throws SQLException {
-        try {
-            conexion = DriverManager.getConnection(ENLACE, USUARIO, CONTRASEÑA);
-            logger.info("Conexión exitosa a la base de datos.");
-        } catch (SQLException e) {
-            logger.log(Level.SEVERE, "Error al conectar a la base de datos: " + e.getMessage(), e);
+    public static ConexionBD obtenerInstancia() throws IOException, SQLException {
+        if (instancia == null) {
+            instancia = new ConexionBD();
         }
+        return instancia;
+    }
+
+    public Connection obtenerConexion() {
         return conexion;
     }
 
-    @Override
-    public void close(){
-        if (conexion != null) {
-            try {
-                conexion.close();
-                logger.info("Conexión cerrada correctamente.");
-            } catch (SQLException e) {
-                logger.log(Level.SEVERE, "Error al cerrar la conexión: " + e.getMessage(), e);
-            }
-        }
-    }
 }
