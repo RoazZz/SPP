c6e63dcdd8e56ed45aadde0aa9116c49ae16f805
RoazZz <goliatcalixto@gmail.com>
2026-04-17 15:07:57 -0600

    refactor: corregir nombrado de IDs, incluir idAdministrador en AdministradorDTO e implementar buscarAdministradorPorId

--- src/main/java/logica/dto/AdministradorDTO.java
+++ src/main/java/logica/dto/AdministradorDTO.java
@@ -4,9 +4,19 @@ import logica.enums.TipoDeUsuario;
 import logica.enums.TipoEstado;
 
 public class AdministradorDTO extends UsuarioDTO{
+    private int idAdministrador;
 
-    public AdministradorDTO(int idUsuario, String nombre, String apellidoP, String apellidoM, String contrasenia, TipoEstado tipoEstado, TipoDeUsuario tipoDeUsuario) {
+    public AdministradorDTO(int idUsuario, String nombre, String apellidoP, String apellidoM, String contrasenia, TipoEstado tipoEstado, TipoDeUsuario tipoDeUsuario, int idAdministrador) {
         super(idUsuario, nombre, apellidoP, apellidoM, contrasenia, tipoEstado, tipoDeUsuario);
+        this.idAdministrador = idAdministrador;
+    }
+
+    public int getIdAdministrador() {
+        return idAdministrador;
+    }
+
+    public void setIdAdministrador(int idAdministrador) {
+        this.idAdministrador = idAdministrador;
     }
 
 }
