2cc7aa1a4f8bb5fe39443fd5a1f257b762801a82
Jared <JaredMT251105@Gmail.com>
2026-04-24 12:57:21 -0600

    Feat: agregar archivo .fxml y controlador de organizacion vinculada

---
+++ src/main/java/gui/controladores/OrganizaciónVinculadaControlador.java
@@ -0,0 +1,38 @@
+package gui.controladores;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import logica.dao.OrganizacionVinculadaDAO;
+import logica.dto.OrganizacionVinculadaDTO;
+
+public class OrganizaciónVinculadaControlador {
+    @FXML
+    private TextField txtIdOrganizacionVinculada;
+    @FXML
+    private TextField txtNombreOrganizacionVinculada;
+    @FXML
+    private TextField txtDireccionOrganizacionVinculada;
+    @FXML
+    private Label txtMensajeGuardar;
+
+    @FXML
+    public void manejarGuardar() {
+        try{
+            String idOrganizacion = txtIdOrganizacionVinculada.getText();
+            String nombre = txtNombreOrganizacionVinculada.getText();
+            String direccion = txtDireccionOrganizacionVinculada.getText();
+
+            OrganizacionVinculadaDTO organizacionVinculadaDTO = new OrganizacionVinculadaDTO(idOrganizacion, nombre, direccion);
+
+            OrganizacionVinculadaDAO organizacionVinculadaDAO = new OrganizacionVinculadaDAO();
+            organizacionVinculadaDAO.agregarOrganizacionVinculada(organizacionVinculadaDTO);
+
+            txtMensajeGuardar.setText("Organización Vinculada guardada con éxito!");
+
+        }catch (Exception e){
+            txtMensajeGuardar.setText("Error: " + e.getMessage());
+        }
+    }
+}
