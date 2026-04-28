41a028f2bef30d2cdb080fb6c5b0c45be3f7a436
RoazZz <goliatcalixto@gmail.com>
2026-04-28 09:26:47 -0600

    feat: agregar preubas que verifiquen al validacion de reglas de negocio

--- src/pruebas/gui/controladores/PruebaFormularioUsuarioControlador.java
+++ src/pruebas/gui/controladores/PruebaFormularioUsuarioControlador.java
@@ -1,6 +1,7 @@
 package gui.controladores;
 
 import accesodatos.ConexionBD;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -89,6 +90,253 @@ class PruebaFormularioUsuarioControlador extends PruebaBaseGUI {
 
         String apellidoPersistido = obtenerApellidoPaternoProfesorPruebaPorNumeroDePersonal(PRUEBA_NUMERO_DE_PERSONAL);
         assertEquals(PRUEBA_APELLIDO_P_PROFESOR, apellidoPersistido);
+    }
+
+    @Test
+    void formularioMuestraErrorCuandoNombreEstaVacio() throws Exception {
+        FxRobot robot = new FxRobot();
+
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#cbTipoUsuario");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("PROFESOR");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("#txtApellidoP").write(PRUEBA_APELLIDO_P_PROFESOR);
+        robot.clickOn("#txtApellidoM").write(PRUEBA_APELLIDO_M_PROFESOR);
+        robot.clickOn("#txtContrasenia").write(PRUEBA_CONTRASENIA);
+        robot.clickOn("#txtNumeroPersonal").write(PRUEBA_NUMERO_DE_PERSONAL);
+        robot.clickOn("#cbTurno");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn(PRUEBA_TURNO);
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("GUARDAR DATOS");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        assertTrue(robot.lookup("#lblError").queryLabeled().isVisible());
+    }
+
+    @Test
+    void formularioMuestraErrorCuandoApellidoPaternoEstaVacio() throws Exception {
+        FxRobot robot = new FxRobot();
+
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#cbTipoUsuario");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("PROFESOR");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("#txtNombre").write(PRUEBA_NOMBRE_PROFESOR);
+        robot.clickOn("#txtApellidoM").write(PRUEBA_APELLIDO_M_PROFESOR);
+        robot.clickOn("#txtContrasenia").write(PRUEBA_CONTRASENIA);
+        robot.clickOn("#txtNumeroPersonal").write(PRUEBA_NUMERO_DE_PERSONAL);
+        robot.clickOn("#cbTurno");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn(PRUEBA_TURNO);
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("GUARDAR DATOS");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        assertTrue(robot.lookup("#lblError").queryLabeled().isVisible());
+    }
+
+    @Test
+    void formularioMuestraErrorCuandoApellidoMaternoEstaVacio() throws Exception {
+        FxRobot robot = new FxRobot();
+
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#cbTipoUsuario");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("PROFESOR");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("#txtNombre").write(PRUEBA_NOMBRE_PROFESOR);
+        robot.clickOn("#txtApellidoP").write(PRUEBA_APELLIDO_P_PROFESOR);
+        robot.clickOn("#txtContrasenia").write(PRUEBA_CONTRASENIA);
+        robot.clickOn("#txtNumeroPersonal").write(PRUEBA_NUMERO_DE_PERSONAL);
+        robot.clickOn("#cbTurno");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn(PRUEBA_TURNO);
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("GUARDAR DATOS");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        assertTrue(robot.lookup("#lblError").queryLabeled().isVisible());
+    }
+
+    @Test
+    void formularioMuestraErrorCuandoContraseniaEstaVacia() throws Exception {
+        FxRobot robot = new FxRobot();
+
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#cbTipoUsuario");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("PROFESOR");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("#txtNombre").write(PRUEBA_NOMBRE_PROFESOR);
+        robot.clickOn("#txtApellidoP").write(PRUEBA_APELLIDO_P_PROFESOR);
+        robot.clickOn("#txtApellidoM").write(PRUEBA_APELLIDO_M_PROFESOR);
+        robot.clickOn("#txtNumeroPersonal").write(PRUEBA_NUMERO_DE_PERSONAL);
+        robot.clickOn("#cbTurno");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn(PRUEBA_TURNO);
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("GUARDAR DATOS");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        assertTrue(robot.lookup("#lblError").queryLabeled().isVisible());
+    }
+
+    @Test
+    void formularioMuestraErrorCuandoContraseniaMenorALongitudMinima() throws Exception {
+        FxRobot robot = new FxRobot();
+
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#cbTipoUsuario");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("PROFESOR");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("#txtNombre").write(PRUEBA_NOMBRE_PROFESOR);
+        robot.clickOn("#txtApellidoP").write(PRUEBA_APELLIDO_P_PROFESOR);
+        robot.clickOn("#txtApellidoM").write(PRUEBA_APELLIDO_M_PROFESOR);
+        robot.clickOn("#txtContrasenia").write("corta");
+        robot.clickOn("#txtNumeroPersonal").write(PRUEBA_NUMERO_DE_PERSONAL);
+        robot.clickOn("#cbTurno");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn(PRUEBA_TURNO);
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("GUARDAR DATOS");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        assertTrue(robot.lookup("#lblError").queryLabeled().isVisible());
+    }
+
+    @Test
+    void formularioMuestraErrorCuandoNumeroDePersonalEstaVacio() throws Exception {
+        FxRobot robot = new FxRobot();
+
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#cbTipoUsuario");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("PROFESOR");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("#txtNombre").write(PRUEBA_NOMBRE_PROFESOR);
+        robot.clickOn("#txtApellidoP").write(PRUEBA_APELLIDO_P_PROFESOR);
+        robot.clickOn("#txtApellidoM").write(PRUEBA_APELLIDO_M_PROFESOR);
+        robot.clickOn("#txtContrasenia").write(PRUEBA_CONTRASENIA);
+        robot.clickOn("#cbTurno");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn(PRUEBA_TURNO);
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("GUARDAR DATOS");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        assertTrue(robot.lookup("#lblError").queryLabeled().isVisible());
+    }
+
+    @Test
+    void formularioMuestraErrorCuandoTurnoNoEstaSeleccionado() throws Exception {
+        FxRobot robot = new FxRobot();
+
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#cbTipoUsuario");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("PROFESOR");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("#txtNombre").write(PRUEBA_NOMBRE_PROFESOR);
+        robot.clickOn("#txtApellidoP").write(PRUEBA_APELLIDO_P_PROFESOR);
+        robot.clickOn("#txtApellidoM").write(PRUEBA_APELLIDO_M_PROFESOR);
+        robot.clickOn("#txtContrasenia").write(PRUEBA_CONTRASENIA);
+        robot.clickOn("#txtNumeroPersonal").write(PRUEBA_NUMERO_DE_PERSONAL);
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("GUARDAR DATOS");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        assertTrue(robot.lookup("#lblError").queryLabeled().isVisible());
+    }
+
+    @Test
+    void formularioNoGuardaProfesorCuandoHayErroresDeValidacion() throws Exception {
+        FxRobot robot = new FxRobot();
+
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#cbTipoUsuario");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("PROFESOR");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        robot.clickOn("GUARDAR DATOS");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        assertTrue(robot.lookup("#lblError").queryLabeled().isVisible());
+        assertTrue(!existeProfesorPruebaConNumeroDePersonal(PRUEBA_NUMERO_DE_PERSONAL));
+    }
+
+    @Test
+    void formularioMuestraErrorCuandoNumeroDePersonalYaExiste() throws Exception {
+        FxRobot robot = new FxRobot();
+
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        llenarFormularioProfesorPrueba(robot);
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("GUARDAR DATOS");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        org.testfx.util.WaitForAsyncUtils.asyncFx(() -> {
+            try {
+                FXMLLoader cargador = new FXMLLoader(getClass().getResource("/gui/vista/FXMLFormularioUsuario.fxml"));
+                Parent raiz = cargador.load();
+                Stage escenarioNuevo = new Stage();
+                escenarioNuevo.setScene(new Scene(raiz));
+                escenarioNuevo.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        org.testfx.util.WaitForAsyncUtils.waitForFxEvents();
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS * 2);
+
+        robot.targetWindow(window -> window.getScene().getRoot().lookup("#cbTipoUsuario") != null);
+
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#cbTipoUsuario");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("PROFESOR");
+
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#txtNombre").write(PRUEBA_NOMBRE_PROFESOR);
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#txtApellidoP").write(PRUEBA_APELLIDO_P_PROFESOR);
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#txtApellidoM").write(PRUEBA_APELLIDO_M_PROFESOR);
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#txtContrasenia").write(PRUEBA_CONTRASENIA);
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#txtNumeroPersonal").write(PRUEBA_NUMERO_DE_PERSONAL);
+
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("#cbTurno");
+        org.testfx.util.WaitForAsyncUtils.waitForFxEvents();
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn(PRUEBA_TURNO);
+
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+        robot.clickOn("GUARDAR DATOS");
+        pausarMilisegundos(PRUEBA_PAUSA_VISUAL_MS);
+
+        assertTrue(robot.lookup("#lblError").queryLabeled().isVisible());
     }
 
     private void llenarFormularioProfesorPrueba(FxRobot robot) {
