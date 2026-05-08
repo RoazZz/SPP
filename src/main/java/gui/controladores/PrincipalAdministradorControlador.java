114dbaebefbafdc4f1ac7587425a5887cd32c9b7
RoazZz <goliatcalixto@gmail.com>
2026-05-08 07:22:34 -0600

    fix: Arreglar la ruta del FXML en la lista de usuarios

--- src/main/java/gui/controladores/PrincipalAdministradorControlador.java
+++ src/main/java/gui/controladores/PrincipalAdministradorControlador.java
@@ -2,14 +2,17 @@ package gui.controladores;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import logica.utilidades.SesionUsuarioSingleton;
 
 public class PrincipalAdministradorControlador extends PrincipalBaseControlador{
 
     @FXML private Button btnUsuarios;
+    @FXML private Button btnCerrarSesion;
 
 
     @Override
     protected void inicializarBotonesEspecificos() {
-        btnUsuarios.setOnAction(e -> abrirVentana("/gui/vista/FXMLGestionUsuarios.fxml"));
+        btnUsuarios.setOnAction(e -> abrirVentana("/gui/vista/FXMLListaUsuarios.fxml"));
     }
+
 }
