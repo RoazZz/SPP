b439e075ed0d762138ef51cdc7514d1e565121ae
RoazZz <goliatcalixto@gmail.com>
2026-05-12 17:32:31 -0600

    feat: agregar atributos y métodos para el hash de los archivos

--- src/main/java/interfaces/ReporteDAOInterfaz.java
+++ src/main/java/interfaces/ReporteDAOInterfaz.java
@@ -2,6 +2,8 @@ package interfaces;
 import excepciones.DAOExcepcion;
 import excepciones.EntidadNoEncontradaExcepcion;
 import logica.dto.ReporteDTO;
+import logica.enums.EstadoReporte;
+import logica.enums.TipoReporte;
 
 import java.util.List;
 
@@ -10,4 +12,6 @@ public interface ReporteDAOInterfaz {
     public boolean actualizarReporte(ReporteDTO reporte) throws DAOExcepcion;
     public ReporteDTO buscarReportePorId(int idReporte) throws DAOExcepcion, EntidadNoEncontradaExcepcion;
     public List<ReporteDTO> listarTodosReporte() throws DAOExcepcion;
+    public boolean existeDuplicado(int idUsuario, TipoReporte tipo, String mes, EstadoReporte estado) throws DAOExcepcion;
+    public boolean existeHashDuplicado(String hashArchivo, String hashContenido) throws DAOExcepcion;
 }
