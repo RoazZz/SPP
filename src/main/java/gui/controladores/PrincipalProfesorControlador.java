67307f58d6c8f454fcb5eb3a4ed1d49f0d890ad2
RoazZz <goliatcalixto@gmail.com>
2026-06-05 00:26:05 -0600

    feat: Agregar calificacion final

--- src/main/java/gui/controladores/PrincipalProfesorControlador.java
+++ src/main/java/gui/controladores/PrincipalProfesorControlador.java
@@ -1,19 +1,17 @@
 package gui.controladores;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import static gui.controladores.NavegacionControlador.abrirVentana;
-
-
 public class PrincipalProfesorControlador extends PrincipalBaseControlador {
     @FXML private Button btnActividades;
     @FXML private Button btnFormatoPresentacion;
     @FXML private Button btnReportes;
-
+    @FXML private Button btnCalificacionFinal;
     @Override
     protected void inicializarBotonesEspecificos() {
-        btnActividades.setOnAction(e -> abrirVentana("/gui/vista/FXMLActividades.fxml", btnActividades));
-        btnFormatoPresentacion.setOnAction(e -> abrirVentana("/gui/vista/FXMLFormatoPresentacion.fxml", btnFormatoPresentacion));
-        btnReportes.setOnAction(e -> abrirVentana("/gui/vista/FXMLListaReportes.fxml", btnReportes));
+        btnActividades.setOnAction(evento -> abrirVentana("/gui/vista/FXMLActividades.fxml", btnActividades));
+        btnFormatoPresentacion.setOnAction(evento -> abrirVentana("/gui/vista/FXMLFormatoPresentacion.fxml", btnFormatoPresentacion));
+        btnReportes.setOnAction(evento -> abrirVentana("/gui/vista/FXMLListaReportes.fxml", btnReportes));
+        btnCalificacionFinal.setOnAction(evento -> abrirVentana("/gui/vista/FXMLCalificacionFinal.fxml", btnCalificacionFinal));
     }
 }
